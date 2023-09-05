aeecfb629b4ad8b20e1c59bceaaaecb589bdd8c6
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-09-05 15:36:33 -0700

    Fetch Pokemon from API
    Add them to state and display them in basic cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Create an array of Pokemon in state
+  const [pokeList, setPokeList] = useState([]);
+  
+  // Grab pokemon info from API
+  const fetchPokemon = async (numOfPoke) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${numOfPoke}`);
+    const pokemon = await response.json();
+    return pokemon;
+  }
+
+  const serializedFetchPokemon = async () => {
+    const newPokeList = []
+    for (let i = 1; i < 11; i++) {
+      const data = await fetchPokemon(i)
+      newPokeList.push({
+        id: data.id,
+        name: data.name,
+        sprite: data.sprites.front_default
+      })
+      
+      console.log(typeof(data.name))
+    }
+
+    setPokeList(newPokeList)
+  }
+
+
+  
+  // Fetch pokemon on page load
+  useEffect(() => {
+    serializedFetchPokemon();
+  }, []);
+
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {pokeList.map((poke) => {
+        return (
+          <div key={poke?.id} className='poke-card'>
+            <img src={poke?.sprite} alt={poke?.name} />
+            <h4>{poke?.name.toUpperCase()}</h4>
+          </div>
+        )
+      })}
     </>
   )
 }
