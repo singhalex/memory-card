0b3cad58713b6671fa0c61574b8fdc245f284e6e
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-09-08 20:47:59 -0700

    Add onClick function

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,8 @@
-const Card = ({name, sprite}) => {
+const Card = ({name, sprite, id, handleClick}) => {
     return (
-        <div className='poke-card'>
+        <div className='poke-card' onClick={() => {
+            handleClick(id)
+        }}>
             <img src={sprite} alt={name} />
             <h4>{name.toUpperCase()}</h4>
         </div>
